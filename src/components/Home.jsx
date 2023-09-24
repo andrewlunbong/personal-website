641727c76c6e2d15357f387fd17e530e188d8c9d
andrewlunbong <andrewlunbong@gmail.com>
2023-09-24 23:19:31 +0100

    minor refactor

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
         <div className="flex flex-col justify-center h-full">
             <h2 className="text-4xl sm:text-7xl font-bold
             text-white">
-            I'm a Full Stack Developer</h2>
+            I'm a backend Stack Developer</h2>
             <p className="text-gray-500 py-4 max-w-md">
                 I have accumulated one year of practical experience in software development and design.
                 Currently, I love to work on web application using technologies like
